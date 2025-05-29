78fd915e8630f320d45c9197b3c595174cdef2bb
Yashi Mittal <yashi.mittal.in@freecharge.com>
2025-05-29 13:12:25 +0530

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "school-cbse-system"
---
+++ src/main/kotlin/com/school/system/SchoolCbseSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.school.system
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SchoolCbseSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<SchoolCbseSystemApplication>(*args)
+}
